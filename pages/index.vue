a13c3f44bb5d176f60a08848fbd5b82e6392dd84
Yacine Djenidi <djenidi.dev@gmail.com>
2023-12-12 01:32:15 +0100

    Up

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,51 @@
 <template>
   <div class="bg-svg-background min-h-screen flex flex-col justify-center">
-    <div class="container mx-auto flex flex-col items-center gap-y-4 md:gap-y-10">
-      <div class="flex w-full md:w-1/2">
-        <input v-model="searchQuery" type="text" placeholder="Search..." class="w-full rounded-lg bg-gray-50 px-4 py-2 outline-none focus:ring-2 focus:ring-blue-400"/>
-        <button @click="searchClicked" class="px-4 py-2 rounded-r-lg bg-blue-400 text-white font-bold hover:bg-blue-500 focus:ring-2 focus:ring-blue-400">Search</button>
+    <div class="text-center mb-20">
+      <span class="px-3 mt-10 mb-4 text-4xl font-bold text-white rounded-lg bg-brand md:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+        Shop Form AliExpress using <span>&#x1F1E9;&#x1F1FF;</span> Dinar!
+      </span>
+    </div>
+    <div class="container mx-auto flex flex-col items-center gap-y-10 md:gap-y-10 px-7">
+      <!-- Input and button -->
+      <div class="flex flex-col md:flex-row w-full md:w-3/4">
+        <input v-model="searchQuery" type="text" placeholder="Search..." class="flex-grow rounded-lg bg-gray-50 px-4 py-2 mb-4 md:mb-0 md:mr-2 outline-none focus:ring-2 focus:ring-blue-400"/>
+        <button @click="searchClicked" class="w-full md:w-auto px-4 py-2 rounded-lg bg-blue-400 text-white font-bold hover:bg-blue-500 focus:ring-2 focus:ring-blue-400">Search</button>
       </div>
 
-      <div v-if="isLoading">
-        <div class="flex flex-row gap-2">
+      <!-- Loading indicator -->
+      <div v-if="isLoading" class="flex justify-center">
+        <div class="flex flex-row gap-3 mt-4">
           <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce"></div>
           <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.3s]"></div>
           <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.5s]"></div>
         </div>
       </div>
 
-      <div v-if="cardData && !isLoading" class="w-full max-w-3xl mx-auto rounded-lg shadow-md bg-white p-4 flex flex-col">
+      <!-- Card data for mobile view -->
+      <div v-if="cardData && !isLoading" class="w-full md:hidden">
+        <div class="bg-white p-4 rounded-lg shadow-md">
+          <div class="flex justify-center mb-4">
+            <div class="w-48 h-48 rounded-lg overflow-hidden">
+              <img :src="cardData.cover" alt="Product Image" class="h-full w-full object-cover">
+            </div>
+          </div>
+          <div class="text-center">
+            <h3 class="text-2xl font-bold text-gray-800 mb-2">{{ cardData.dzPrice }}</h3>
+            <p class="text-gray-700 mb-2">{{ cardData.name }}</p>
+            <!-- Other details for mobile -->
+            <p class="text-gray-700"><span>&#x1F389;</span> Sales: {{ cardData.sales }} </p>
+            <p class="text-gray-700"><span>&#x1F3E2;</span> Store: {{ cardData.store }} </p>
+            <p class="text-gray-700"><span>&#x2B50;</span> Store Rate: {{ cardData.storeRate }} </p>
+          </div>
+          <div class="flex justify-center space-x-7 mt-4">
+            <a href="https://m.me/100093485946734" class="px-4 py-2 rounded-lg bg-blue-500 text-white font-bold hover:bg-blue-600 focus:ring-2 focus:ring-blue-400"> Messenger</a>
+            <a href="https://wa.me/+213663712471" class="px-4 py-2 rounded-lg bg-green-400 text-white font-bold hover:bg-green-500 focus:ring-2 focus:ring-blue-400"> WhatsApp</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card data for desktop view -->
+      <div v-if="cardData && !isLoading" class="w-full max-w-3xl mx-auto rounded-lg shadow-md bg-white p-4 md:p-6 hidden md:block">
         <div class="flex w-full items-center">
           <div class="h-48 w-48 rounded-lg object-cover flex-shrink-0">
             <img :src="cardData.cover" alt="Product Image" class="h-full w-full rounded-lg object-cover">
@@ -34,8 +65,7 @@
                 <p class="text-sm text-gray-700 "> ({{ cardData.totalRates }})  </p>
               </div>
               <p class="text-gray-700 mb-2">{{ cardData.name }}</p>
-              <!-- New Details -->
-              <p class="text-gray-700"><span>&#11088;</span> Total Rates: {{ cardData.totalRates }} </p>
+              <!-- Other details for desktop -->
               <p class="text-gray-700"><span>&#x1F389;</span> Sales: {{ cardData.sales }} </p>
               <p class="text-gray-700"><span>&#x1F3E2;</span> Store: {{ cardData.store }} </p>
               <p class="text-gray-700"><span>&#x2B50;</span> Store Rate: {{ cardData.storeRate }} </p>
@@ -51,6 +81,7 @@
     </div>
   </div>
 </template>
+
 
 
 <script setup>
